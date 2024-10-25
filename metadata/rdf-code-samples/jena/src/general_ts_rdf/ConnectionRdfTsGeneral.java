e1d131a12c0a013e9607b027bb3b8a65ee4cf710
York Earwaker <yorkearwaker@yahoo.com>
2024-10-25 15:41:20 +0100

    todo second

---
+++ metadata/rdf-code-samples/jena/src/general_ts_rdf/ConnectionRdfTsGeneral.java
@@ -0,0 +1,29 @@
+package general_ts_rdf;
+
+
+/*
+** RDF Connection to some triple store
+** Connect to RDF triple store general, which might be any triple store, with Jena RDF Connection: SPARQL operations API, using HTTP and SPARQL protocols
+** 
+** <todo: use generic means to access local Fuseki rdf triple store instance, >
+** <todo: use generic means to access A N Other rdf tirple store instance, DBPedia? Other online, another class? >
+** <todo: attempt to access non native rdf store with with sparql 1.1 over http, >
+*/
+public class ConnectionRdfTsGeneral extends Object {
+	
+	public static void main(String args[]) {
+		
+		System.out.println("ConnectionRdfTsGeneral in ");
+		
+		System.out.println("Stub <todo: create general rdf triple store connection implementation. > ");
+		
+		System.out.println("ConnectionRdfTsGeneral out ");
+		
+		
+	}
+	
+	// Fuseki connection to local instance; 1 local tomcat .war instnace, 2 local cmd line instance
+	
+	// dbpedia connection, remote https://www.dbpedia.org/resources/sparql/
+	
+}
