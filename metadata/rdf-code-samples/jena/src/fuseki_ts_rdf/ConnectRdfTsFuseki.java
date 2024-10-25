69280024dc4e0328ead70f9f8c7a818d4b9abcb8
York Earwaker <yorkearwaker@yahoo.com>
2024-10-25 15:40:04 +0100

    todo first

---
+++ metadata/rdf-code-samples/jena/src/fuseki_ts_rdf/ConnectRdfTsFuseki.java
@@ -0,0 +1,21 @@
+package fuseki_ts_rdf;
+
+/*
+** RDF Connection specifically for Fuseki
+** Connect to RDF triple store Apache Fuseki with Jena RDF Connection: SPARQL operations API, using HTTP and SPARQL protocols
+*/
+public class ConnectRdfTsFuseki extends Object {
+	
+	public static void main(String arg[]) {
+		
+		System.out.println("ConnectRdfTsFuseki in ");
+		
+		System.out.println("Stub <todo: create Fuseki specific rdf triple store connection implementation. > ");
+		
+		System.out.println("ConnectRdfTsFuseki out ");
+		
+	}
+	
+	// Fuseki connection to local instance; 1 local tomcat .war instnace, 2 local cmd line instance
+	
+}
